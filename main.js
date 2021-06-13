16868d2e31246f0e03fbcbd92586cba26b1732b1
Marina Burtseva <9115131201@mail.ru>
2021-06-13 22:57:30 +0300

    изм

--- main.js
+++ main.js
@@ -34,4 +34,33 @@ $('.header__button').on('click', function() {
   $('.close__popup').on('click', function() {
     $('.appoint').removeClass('active');
   });
-  // 
+  
+// Работа якоря
+$(function() {
+  $('.arrow').click(function(){
+      $('html, body').animate({scrollTop:0}, 'slow');
+  });
+});
+
+
+// Плавный скролл для ссылок
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = document.querySelector('.scrollto').offsetHeight;
+      // const topOffset = 0; // если не нужен отступ сверху 
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
