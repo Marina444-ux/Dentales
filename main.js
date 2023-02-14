abf1d289c9f4efb9183079113eb7255519dc5bb0
Marina Burtseva <9115131201@mail.ru>
2023-02-14 19:11:34 +0300

    hhhhhh

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ $('.header__button').on('click', function() {
   });
   
 
-  $('.form-btn').on('click', function() {
-    evt. preventDefault();
-    $('.popups__inner').removeClass('active');
-    $('.successful') .addClass('activ');
-  });
+  // $('.form- btn').on('click', function() {
+  //   evt. preventDefault();
+  //   $('.popups__inner').removeClass('active');
+  //   $('.successful') .addClass('activ');
+  // });
   
-  $('.close__popup').on('click', function() {
-    $('.successful').removeClass('active');
-  });
+  // $('.close__popup').on('click', function() {
+  //   $('.successful').removeClass('active');
+  // });
 
   $('.sectioh__left-btn').on('click', function() {
     $('.consult').addClass('active');
